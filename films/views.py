d54e2581b07509851991fa2a83762fbf5a12d755
MRROBOT <urmanov.2000@mail.ru>
2023-05-19 18:32:36 +0800

    This is my first project

---
+++ films/views.py
@@ -0,0 +1,128 @@
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+
+from .models import Movie, Actor, Genre, Rating
+from .forms import ReviewForm, RatingForm
+
+
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MoviesView(GenreYear, ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    paginate_by = 3
+
+
+class MovieDetailView(GenreYear, DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = 'url'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["star_form"] = RatingForm()
+        context["form"] = ReviewForm()
+        return context
+
+    def get_user_stars(self, ip, movie_id):
+        if Rating.objects.filter(ip=ip, movie_id=movie_id).exists():
+            stars = Rating.objects.get(ip=ip, movie_id=movie_id).star
+        else:
+            stars = None
+        return stars
+
+    def get(self, request, *args, **kwargs):
+        ip = AddStarRating.get_client_ip(self, self.request)
+        movie_id = Movie.objects.get(url=kwargs['slug']).id
+        stars = self.get_user_stars(ip, movie_id)
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        if stars:
+            context['stars'] = str(stars)
+        return self.render_to_response(context)
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            # Ищем в POST запросе ключ parent, то есть имя нашего поля
+            if request.POST.get('parent', None):
+                # Добавляем именно число, а не объект
+                form.parent_id = int(request.POST.get('parent'))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
+
+
+class ActorView(GenreYear, DetailView):
+    """Вывод информации об актере"""
+    model = Actor
+    template_name = 'films/actor.html'
+    slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтр фильмов"""
+    paginate_by = 3
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        ).distinct()
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context= super().get_context_data(*args, **kwargs)
+        context['year'] = ''.join([f'year={x}&' for x in self.request.GET.getlist('year')])
+        context['genre'] = ''.join([f'genre={x}&' for x in self.request.GET.getlist('genre')])
+        return context
+
+class AddStarRating(View):
+    """Добавление рейтинга фильму"""
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def post(self, request):
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            Rating.objects.update_or_create(
+                ip=self.get_client_ip(request),
+                movie_id=int(request.POST.get("movie")),
+                defaults={'star_id': int(request.POST.get("star"))}
+            )
+            return HttpResponse(status=201)
+        else:
+            return HttpResponse(status=400)
+
+
+class Search(ListView):
+    """Поиск фильмов"""
+    paginate_by = 3
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["q"] = f'{self.request.GET.get("q")}&'
+        return context
